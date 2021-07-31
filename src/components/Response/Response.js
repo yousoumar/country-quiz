52e066fb7d17aa5a159a824bd32386eccedb2d20
yousoumar <oumaiyr@gmail.com>
2021-07-31 21:42:02 +0200

    updates readme.md

--- src/components/Response/Response.js
+++ src/components/Response/Response.js
@@ -2,27 +2,27 @@ import {useRef, useState} from 'react';
 export default function Response({name, index, correctResponse, possibleShownResponses, addToRef, countriesSetState,  countriesState, newQuestion,possibleResponsesState, possibleResponsesSetState, firstTestState, firstTestSetState }) {
     // to store score
     let score = useRef(5).current;
-    
+
     function checkResponse(element) {
         
         if(firstTestState){
             firstTestSetState(false);
             let userResponse = element.children[1].innerText;
-                if (userResponse === correctResponse.name){
-                    element.classList.add('correct');
-                    element.querySelector('img').src = process.env.PUBLIC_URL + '/images/correct.svg'
-                    
-                }else{
-                    score= score-1;
-                    element.classList.add('incorrect');
-                    element.querySelector('img').src = process.env.PUBLIC_URL + '/images/incorrect.svg';
-                    possibleShownResponses.forEach(element => {
-                        if (element.children[1].innerText === correctResponse.name){
-                            element.classList.add('correct');
-                            element.querySelector('img').src = process.env.PUBLIC_URL + '/images/correct.svg'
-                        }
-                    });
-                }
+            if (userResponse === correctResponse.name){
+                element.classList.add('correct');
+                element.querySelector('img').src = process.env.PUBLIC_URL + '/images/correct.svg'
+                
+            }else{
+                score= score-1;
+                element.classList.add('incorrect');
+                element.querySelector('img').src = process.env.PUBLIC_URL + '/images/incorrect.svg';
+                possibleShownResponses.forEach(element => {
+                    if (element.children[1].innerText === correctResponse.name){
+                        element.classList.add('correct');
+                        element.querySelector('img').src = process.env.PUBLIC_URL + '/images/correct.svg'
+                    }
+                });
+            }
         }else{
             return
         }
